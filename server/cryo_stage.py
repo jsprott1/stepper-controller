928b79e31a1a97ecbbc7f11f4b78fc48262c66db
Jack Sprott <jacksprott@proton.me>
2025-08-21 14:16:32 +0200

    Updated to latest version on transfer

--- server/cryo_stage.py
+++ server/cryo_stage.py
@@ -1,7 +1,7 @@
 import time
-# import numpy as np
+import numpy as np
 import json
-from flask import Flask, request
+from flask import Flask, request, abort
 from stage_firmware import Stage
 
 class CryoStepperMicroservice:
@@ -11,52 +11,86 @@ class CryoStepperMicroservice:
         #shared data updated by the api
         self.flask_app = Flask(__name__)
         self.filter_ip = bool(int(self.config["stepper_controller_api"]["restrict_network_partner"]))
-        self.is_busy = False
-        self.stepper_controller = Stage(self.config["stepper_controller"])
-        for axis in self.config["stepper_controller"]["axes"]:
-            self.stepper_controller.home(axis)
+        self.init_controller()
         
-        @self.flask_app.route(self.config["route"], methods=['POST'])
-        def flask_receive_data(self):
+        @self.flask_app.route(self.config["stepper_controller_api"]["route"] + "/post", methods=['POST'])
+        def flask_receive_data():
             new_data = request.get_json()
             addr = request.remote_addr
-            if self.filter_ip and addr != self.config["allowed_network_partner"]:
+            if self.filter_ip and addr != self.config["stepper_controller_api"]["allowed_network_partner"]:
                 abort(403)
+            elif "reset" in new_data and new_data["reset"]:
+                self.close()
+                self.init_config()
+                self.init_controller(home=False)
+                return "reset stepper motors"
+            elif "config" in new_data:
+                self.update_config(new_data["config"], new_data["config_data"], new_data["by_factor"])
             elif self.is_busy:
                 abort(409, description="device busy")
             
             # device specific stuff
-            if "absolute_position" in new_data:
-                self.is_busy = True
-                self.stepper_controller.moveTo_mm_vec(new_data["absolute_position"], velocity=new_data["velocity"])
-                self.is_busy = False
-                return "moved to " + stepper_controller.get_position()
-            elif "relative_position" in new_data:
-                self.is_busy = True
-                self.stepper_controller.moveTo_mm_vec(new_data["relative_position"]+self.stepper_controller.get_position(), velocity=new_data["velocity"])
-                self.is_busy = False
-                return "moved to " + stepper_controller.get_position()
-            elif "home" in new_data and new_data["home"]:
+            if "home" in new_data and new_data["home"]:
                 self.is_busy = True
                 self.stepper_controller.home(new_data["axis"])
                 self.is_busy = False
                 return "homed " + new_data["axis"] + " axis"
+            elif "absolute_position" in new_data:
+                self.is_busy = True
+                target_position = np.array(new_data["absolute_position"])
+                delta = target_position - self.stepper_controller.get_position()
+            elif "relative_position" in new_data:
+                self.is_busy = True
+                delta = np.array(new_data["relative_position"])
+                target_position = delta + self.stepper_controller.get_position()
+            elif "calibrate_backlash" in new_data:
+                return self.stepper_controller.calibrate_backlash(new_data["calibrate_backlash"][0], new_data["calibrate_backlash"][1], new_data["velocity"])
             else:
                 abort(400)
                 
-        @self.flask_app.route(self.config["route"], methods=['GET'])
-        def flask_request_data(self):
-            return {"position": stepper_controller.get_position(), "busy": self.is_busy}
+            if "backlash_correction" in new_data and new_data["backlash_correction"]:
+                b_inds = np.full_like(delta, 0)
+                for i, d in enumerate(delta):
+                    if d > 0:
+                        b_inds[i] = 1
+                self.stepper_controller.move_to_mm_vec(target_position + 0.1*delta*b_inds, velocity=new_data["velocity"])
+            self.stepper_controller.move_to_mm_vec(target_position, velocity=new_data["velocity"])
+            self.is_busy = False
+            return "moved to " + str(self.stepper_controller.get_position())
+                
+            
+                
+        @self.flask_app.route(self.config["stepper_controller_api"]["route"] + "/get", methods=['GET'])
+        def flask_request_data():
+            return {"position": self.stepper_controller.get_position().tolist(), "busy": self.is_busy}
         
     def run(self):
-        self.flask_app.run()
+        self.flask_app.run(host=self.config["stepper_controller_api"]["flask_listen_ip"], port=self.config["stepper_controller_api"]["server_port"])
         
     def init_config(self):
         with open(self.conf_fname, 'r') as file:
             self.config = json.load(file)
             
+    def update_config(self, path, value, by_factor):
+        element = self.config
+        for ind in path[:-1]:
+            element = element[ind]
+        if by_factor:
+            element[path[-1]] *= value
+        else:
+            element[path[-1]] = value
+        with open(self.conf_fname, 'w', encoding='utf-8') as file:
+            json.dump(self.config, file, ensure_ascii=False, indent=4)
+        
+            
+    def init_controller(self, home=True):
+        self.is_busy = False
+        self.stepper_controller = Stage(self.config["stepper_controller"])
+        if home:
+            for axis in self.config["stepper_controller"]["axes"]:
+                self.stepper_controller.home(axis)
+            
     def close(self):
         self.stepper_controller.close()
             
-if __name__ == '__main__':
-    cryo_microservice = CryoStepperMicroservice()
+
--- server/stage_firmware.py
+++ server/stage_firmware.py
@@ -14,9 +14,14 @@ class Stage:
         self.motors = self.config["motors"]
         self.axis_bounds = self.config["limits"]
         self.microsteps = self.config["microsteps"]
-        self.stepsPerMm = self.config["steps_per_mm"] * self.microsteps
+        self.steps_per_mm = self.config["steps_per_mm"]# In format of {axis:[-,+]}
+        self.steps_per_mm.update((x, np.array(y)*self.microsteps) for x, y in self.steps_per_mm.items())
+        self.max_velocity = min(self.config["max_velocity"] * self.microsteps, 1000)
+        self.max_current = self.config["max_current"]
+        self.current_position = np.zeroes(self.axes)
         self.initialize_interface()
         self.initialize_motors()
+
         
         
     def initialize_interface(self):
@@ -27,17 +32,19 @@ class Stage:
     def initialize_motors(self):
         self.module.set_global_parameter(self.module.GP0.reverseShaftDirection, 0, 3) # Reverses direction of all motors
         # Preparing parameters
-        for motor_i in self.motors.values():
+        for i, motor_i in enumerate(self.motors):
             motor = self.module.motors[motor_i]
-            motor.drive_settings.run_current = 152
-            motor.drive_settings.max_current = 175
+            motor.drive_settings.run_current = min(self.max_current[i], 152)
+            motor.drive_settings.max_current = motor.drive_settings.run_current
             motor.drive_settings.standby_current = 0
             motor.drive_settings.microstep_resolution = self.microStepDict[self.microsteps]
             motor.drive_settings.freewheeling_delay = 1
             motor.max_acceleration = min(5 * self.microsteps, 500)
-            motor.max_velocity = min(self.config["max_velocity"] * self.microsteps, 1000)
+            motor.max_velocity = self.max_velocity
             motor.linear_ramp.max_acceleration = int(motor.max_acceleration) 
             motor.linear_ramp.max_velocity = int(motor.max_velocity)
+            
+            motor.min_velocity = self.config["min_velocity"]
             
     def home(self,axis):
         axis_i = self.axes.index(axis)
@@ -55,30 +62,39 @@ class Stage:
             final_pos = self.module.get_axis_parameter(self.module.motors[motor].AP.ActualPosition, motor)
             if vidx == 1 and final_pos == initial_pos :
                 print("WARNING: Limit switch may be disconnected")
-            self.module.motors[motor].actual_position = home_pos * self.stepsPerMm[axis_i] 
+            self.module.motors[motor].actual_position = round(home_pos * self.steps_per_mm[axis][0]) 
+            self.current_position[axis_i] = home_pos
             self.initialize_motors()
             if vidx == 0:
-                self.module.move_to(motor,round((home_pos - 1 * home_dir) * self.stepsPerMm[axis_i]))
+                self.move_to_mm(axis,home_pos - 1 * home_dir)
                 while not self.module.get_axis_parameter(self.module.motors[motor].AP.PositionReachedFlag,motor):
                     pass
             time.sleep(0.3)
-        self.moveTo_mm(axis,home_pos)
+        self.move_to_mm(axis,home_pos)
         print("stage homed!")
         
     
     def move_to(self,axis, position, velocity=None):
-        self.module.move_to(self.motors[self.axes.index(axis)],position,velocity)
+        motor = self.motors[self.axes.index(axis)]
+        if velocity == None:
+            velocity = self.module.motors[motor].max_velocity
+        if velocity > self.max_velocity:
+            velocity = self.max_velocity
+        self.module.move_to(motor,position,velocity)
         
     def move_to_mm(self,axis, position_mm, velocity=None, blocking=True):
         if position_mm < self.axis_bounds[axis][0] or position_mm > self.axis_bounds[axis][1]:
             raise Exception(f"Axis bounds error. position {position_mm}mm is out of the allowed range for axis {axis}.")
         axis_i = self.axes.index(axis)
-        self.move_to(axis,round(position_mm * self.stepsPerMm[axis_i]),velocity)
+        delta = position_mm - self.current_position[axis_i]
+        delta_steps = round(delta * self.steps_per_mm[axis][delta > 0])
+        self.move_to(axis,delta_steps + self.get_motor_position(axis),velocity)
         if blocking:
             while not self.module.get_axis_parameter(self.module.motors[self.motors[axis_i]].AP.PositionReachedFlag,self.motors[axis_i]):
                 pass
+        self.current_position[axis_i] += delta_steps/self.steps_per_mm[axis][delta > 0]
 
-    def moveTo_mm_vec(self, pos_mm_vec, velocity=None, blocking=True):
+    def move_to_mm_vec(self, pos_mm_vec, velocity=None, blocking=True):
         self.move_to_mm(self.axes[0], pos_mm_vec[0], velocity, False)
         self.move_to_mm(self.axes[1], pos_mm_vec[1], velocity, False)
         if blocking:
@@ -87,14 +103,40 @@ class Stage:
     
     def positionReachedFlag(self):
         flags = []
-        for motor in self.motors.values():
+        for motor in self.motors:
             flags.append(self.module.get_axis_parameter(self.module.motors[motor].AP.PositionReachedFlag,motor))
         if sum(flags) == len(flags):
             return True
         return False
+    
+    def get_position(self):
+        return np.array(self.current_position)
+        
+    def get_motor_position(self, axis):
+        return self.module.motors[self.motors[self.axes.index(axis)]].actual_position
         
     def close(self):
         self.myInterface.close()
+        
+    # Returns distance recorded returning to limit after moving $distance away
+    def calibrate_backlash(self, axis, distance, velocity=None):
+        if velocity == None:
+            velocity = 200
+        axis_i = self.axes.index(axis)
+        motor = self.motors[axis_i]
+        home_pos = self.config["home"][axis_i]
+        home_dir = self.config["home_direction"][axis_i]
+        
+        self.home(axis)
+        self.move_to_mm(axis, home_pos - distance*home_dir, velocity)
+        initial_pos = self.get_position()[axis_i]
+        self.module.rotate(motor, velocity*home_dir)
+        while not self.module.get_axis_parameter(self.module.motors[motor].AP.LeftEndstop,motor):
+            pass
+        self.module.rotate(motor, 0)
+        final_pos = self.get_position()[axis_i]
+        return str(abs(final_pos - initial_pos))
+        
         
 
 
